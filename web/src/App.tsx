961ec188ce5712b5eb93bdea36c59513ca8b5210
Christiano Marques <8249890+christianodejesus@users.noreply.github.com>
2020-06-03 03:09:58 -0300

    First commit on web spa react project

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>NLW - Rocketseat</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
